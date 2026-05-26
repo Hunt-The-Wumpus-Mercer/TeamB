6c3bb6177a54af37c115184a55fad5dd7a6e9f24
lucyhao-lab <lucy.hao@misd400.org>
2026-05-26 16:25:33 +0000

    addign trivia

---
+++ src/components/trivia/Trivia.ts
@@ -0,0 +1,15 @@
+import type { ITrivia, QuestionPrompt } from "./ITrivia";
+
+export default class trivia implements ITrivia {
+    initialize(): Promise<void> {
+        
+    }
+
+    getNextQuestion(): QuestionPrompt {
+        
+    }
+
+    getHint(): string {
+        
+    }
+}
