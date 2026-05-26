df2367585a7c2f93d3bca88fc7096e29d7e82840
avalele <ava.lele@misd400.org>
2026-05-26 16:20:54 +0000

    implement map

---
+++ src/components/Map_and_PLayer/Map.ts
@@ -0,0 +1,39 @@
+import { type IMap,MapObjectType }from "./IMap"
+
+export default class Map implements IMap {
+    player = 0;
+    bat1 = 0;
+    bat2 = 0;
+    pit1=0;
+    pit2 = 0;
+    wumpus = 0;
+
+    /**
+         * Returns the room location for the requested map object.
+         */
+        getRoomLocation(type: MapObjectType): number{
+            return 1;
+        }
+    
+        /**
+         * Sets the room location for the requested map object.
+         */
+        setRoomLocation(type: MapObjectType, roomNumber: number): void{
+            
+            if(type === MapObjectType.PLAYER){
+                this.player=roomNumber;
+            }
+            else if(type === MapObjectType.BAT1){
+                this.bat1 = roomNumber;
+            }
+            else if(type === MapObjectType.BAT2){
+                this.bat2 = roomNumber;
+            }else if(type === MapObjectType.PIT1){
+                this.pit1 === roomNumber;
+            }else if(type === MapObjectType.PIT2){
+                this.pit2 === roomNumber;
+            }else if(type === MapObjectType.WUMPUS){
+                this.wumpus = roomNumber;
+            }
+        }
+}
