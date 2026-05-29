ad4644bfef097c0aa29da7519fe72acc47a826e5
arialele1234 <aria.lele@misd400.org>
2026-05-29 15:51:39 +0000

    get game control started

--- src/components/game_control/IGameControl.ts
+++ src/components/game_control/IGameControl.ts
@@ -5,7 +5,7 @@ export interface IGameControl {
     /**
      * Initializes all game dependencies and renders the game UI.
      */
-    init(containerSelector: string): Promise<void>;
+    init(containerSelector: JQuery): void;
 
     /**
      * Runs a trivia challenge and returns the challenge outcome.
@@ -15,25 +15,24 @@ export interface IGameControl {
     /**
      * Attempts to move the player one room in the specified direction.
      */
-    movePlayer(caveRoomDirection: CaveRoomDirections): Promise<string>;
-
+    movePlayer(caveRoomDirection: CaveRoomDirections): void;
     /**
      * Shoots an arrow through the selected doorway direction.
      */
-    shootArrow(caveRoomDirection: CaveRoomDirections): Promise<string>;
+    shootArrow(caveRoomDirection: CaveRoomDirections):void;
 
     /**
      * Attempts to buy arrows by completing a trivia challenge.
      */
-    purchaseArrow(): Promise<string>;
+    purchaseArrow():void;
 
     /**
      * Attempts to buy a secret by completing a trivia challenge.
      */
-    purchaseSecret(): Promise<string>;
+    purchaseSecret():void;
 
     /**
      * Displays the high scores.
      */
-    viewHighScores(): Promise<string>;
+    viewHighScores():void;
 }
--- src/wumpusMain.ts
+++ src/wumpusMain.ts
@@ -1,17 +1,18 @@
 
 import $ from "jquery";
 import "./style.css";
-import { GameControl } from "./components/game_control/GameControl";
+import  GameControl  from "./components/game_control/GameControl";
 
 const $app = $("#app");
 $app.html(`
 	<div>
 		<h1>Hunt the Wumpus</h1>
 		<div id="WumpusGameContainer"></div>
+		<div id="test"></div>
 	</div>
 `);
 
 const gameControl = new GameControl();
-
-void gameControl.init("#WumpusGameContainer");
+const container = $('#WumpusGameContainer');
+void gameControl.init(container);
 
