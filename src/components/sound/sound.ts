38e8e56d63afa7bafc53fead3e38b3f1985a39df
chasegreenfield-commits <chase.greenfield@misd400.org>
2026-05-28 22:00:50 -0700

    Update sound.ts

--- src/components/sound/sound.ts
+++ src/components/sound/sound.ts
@@ -8,31 +8,31 @@ export default class SoundManager implements ISoundManager {
         
         if(soundEventType == SoundEventType.WALK)
         {
-            path = "sound/u_3x9ga8wevj-walking-sound-effect-272246.mp3";
+            path = "";
         }
         else if(soundEventType == SoundEventType.SHOOT_ARROW)
         {
-            path = "src/components/sound/djartmusic-arrow-swish_03-306040.mp3";
+            path = "";
         }
         else if(soundEventType == SoundEventType.WIN)
         {
-            path = "src/components/sound/mrstokes302-you-win-sfx-mrstokes302-442128.mp3";
+            path = "";
         }
         else if(soundEventType == SoundEventType.LOSE)
         {
-            path = "src/components/sound/mrstokes302-you-lose-sfx-mrstokes302-528744.mp3";
+            path = "";
         }
         else if(soundEventType == SoundEventType.WARNING_BAT)
         {
-            path = "src/components/sound/dragon-studio-bird-wings-463212.mp3";
+            path = "";
         }
         else if(soundEventType == SoundEventType.WARNING_PIT)
         {
-            path = "src/components/sound/dragon-studio-droplets-in-a-cave-482871.mp3";
+            path = "";
         }
         else if(soundEventType == SoundEventType.WARNING_WUMPUS)
         {
-            path = "src/components/sound/dragon-studio-monster-growl-376892.mp3";
+            path = "";
         }
 
         // Only play if a matching path was found
